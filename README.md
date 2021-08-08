79170a433fdb10bb18144dfaf5cb43e3b7ec5f07
ariel <ariel.ep91@gmail.com>
2021-08-08 01:56:53 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Testing + TDD
+
+Este proyecto nos servirá para ejercitar la técnica de TDD.
+
+![](imgs/tdd.png)
---
+++ src/main/java/com/redbee/clase5/Clase5Application.java
@@ -0,0 +1,13 @@
+package com.redbee.clase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase5Application.class, args);
+    }
+
+}
